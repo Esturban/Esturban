91154b00856b577d96dbd9853f10983fa105ef1b
Esturban <valtaye@gmail.com>
2021-07-01 12:58:53 -0400

    Front matter
    Updating the front matter to include github stats and my online real person stuff

--- update/main.go
+++ update/main.go
@@ -29,10 +29,14 @@ func makeReadme(filename string) error {
 	date := time.Now().Format("2 Jan 2006")
 
 	// Whisk together static and dynamic content until stiff peaks form
-	hello := "Hello there 👋"
+	nav := "# <video autoplay="" height="32" data-vscid="wxo1z48g0"><source src="https://www.estebanvalencia.com/me/tux.webm" type="video/webm"></video> Esteban Valencia"
+	header := "## Engineer / Developer / Product Development / Traveler"
+	ghs := "[![Esturban's GitHub stats](https://github-readme-stats.vercel.app/api?username=Esturban&count_private=true&show_icons=true&theme=algolia)](https://github.com/anuraghazra/github-readme-stats)"
+    lk := "<a href="https://www.linkedin.com/in/valest/" target="_blank"><img src="https://img.shields.io/badge/linkedin-valest-blue?style=for-the-badge&logo=linkedin" /></a>"
+    social := lk
 	blog := "<p align=\"center\"><img src=\"https://media.giphy.com/media/BTuF8rhHsnosLQPpGB/giphy.gif\" alt = \"ZTFO\"/></p>"
 	updated := "<sub>Last updated by magic on " + date + ".</sub>"
-	data := fmt.Sprintf("%s\n\n%s\n\n%s\n", hello, blog, updated)
+	data := fmt.Sprintf("%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n", nav, header, ghs, social, blog, updated)
 
 	// Prepare file with a light coating of os
 	file, err := os.Create(filename)
