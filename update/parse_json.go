ccf7b5dba2b928230f5e8879669ea8e94b9a741e
Esturban <valtaye@gmail.com>
2021-07-01 19:51:35 -0400

    Revert
    Changing the code to the portfolio page to reflect the markdown component
    
    Couldn't center it well
    
    wamp wamp

--- update/parse_json.go
+++ update/parse_json.go
@@ -2,12 +2,28 @@ package main
 
 import (
     "encoding/json"
-    "log"
+    "fmt"
+    "io/ioutil"
+    "os"
 )
 
 func main() {
-    dataJson, err := os.Open(".json")
-    var arr []string
-    _ = json.Unmarshal([]byte(dataJson), &arr)
-    log.Printf("Unmarshaled: %v", arr)
+
+    // Open our jsonFile
+    jsonFile, err := os.Open("C:/Users/evalencia/OneDrive/01_dataprojects/_tasks/evdotcom_20190923/_site/data/quotes.json")
+    // if we os.Open returns an error then handle it
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Successfully Opened quotes.json")
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    var result map[string]interface{}
+    json.Unmarshal([]byte(byteValue), &result)
+
+    fmt.Println(result["0"])
+
 }
